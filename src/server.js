f9b1a75ccf9d6f684e5df73f2b49f989c514e890
Joao <joaommattedi@gmail.com>
2023-07-12 20:23:38 -0300

    get/post route

--- src/server.js
+++ src/server.js
@@ -1,7 +1,27 @@
 import http from 'node:http'
 
+const users = [];
+
 const server = http.createServer((req,res) => {
-  return res.end('Hello World')
+
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/user') {
+    return res
+      .setHeader('Content-type','application/json')
+      .end(JSON.stringify(users))
+  }
+  if (method === 'POST' && url === '/user') {
+    users.push({
+      id: 1,
+      name: 'Johnzera',
+      email: 'john@john.com'
+    })
+
+    return res.writeHead(201).end('Usuário criado')
+  }
+
+  return res.writeHead(404).end('NOT FOUND')
 })
 
 server.listen(3333)
