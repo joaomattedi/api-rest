0f87ca53458e97ddb1583a1f07f7dc72fc08aa21
Joao <joaommattedi@gmail.com>
2023-07-13 20:11:13 -0300

    streams learnings

--- src/server.js
+++ src/server.js
@@ -2,9 +2,21 @@ import http from 'node:http'
 
 const users = [];
 
-const server = http.createServer((req,res) => {
+const server = http.createServer(async (req, res) => {
 
-  const { method, url } = req
+  const { method, url } = req;
+
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch (e) {
+    req.body = null
+  }
 
   if (method === 'GET' && url === '/user') {
     return res
@@ -12,10 +24,12 @@ const server = http.createServer((req,res) => {
       .end(JSON.stringify(users))
   }
   if (method === 'POST' && url === '/user') {
+    const {nome,email} = req.body
+
     users.push({
       id: 1,
-      name: 'Johnzera',
-      email: 'john@john.com'
+      name: nome,
+      email: email
     })
 
     return res.writeHead(201).end('Usuário criado')
